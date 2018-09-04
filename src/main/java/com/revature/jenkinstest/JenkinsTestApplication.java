b1c4107a576c5f1707999f11a4616eb0d5e592ba
timtouch <chaosbounder@gmail.com>
2018-09-04 13:42:41 -0500

    Changed pom.xml

--- src/main/java/com/revature/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/revature/jenkinstest/JenkinsTestApplication.java
@@ -2,11 +2,12 @@ package com.revature.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class JenkinsTestApplication
+public class JenkinsTestApplication extends SpringBootServletInitializer
 {
 
     public static void main(String[] args)
